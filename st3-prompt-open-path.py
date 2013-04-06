399e4d1b60a5f2b83347768dfafa7225fdb1fb77
Chris Guilbeau <chris.guilbeau@gmail.com>
2013-04-06 01:27:27 -0500

    ~ addressed bug where autocomplete would take over if you typed something that already existed in the buffer

--- st3-prompt-open-path.py
+++ st3-prompt-open-path.py
@@ -17,6 +17,7 @@ from sublime import Region
 from sublime import status_message
 from sublime_plugin import TextCommand
 from sublime_plugin import WindowCommand
+from sublime_plugin import EventListener
 
 class CompletePath(TextCommand):
     def run(self, edit, text):
@@ -33,6 +34,7 @@ class CompletePath(TextCommand):
         # Add a trailing slash if you are tabbing into a directory
         if len(options) == 1 and isdir(newText):
             newText = join(newText, '')
+        print(newText)
         panel.replace(edit, region, newText)
 
 class St3PromptOpenPath(WindowCommand):
@@ -40,19 +42,21 @@ class St3PromptOpenPath(WindowCommand):
     def run(self):
         window = self.window
         activeView = self.window.active_view()
+        # turn off tab completion
+        activeView.settings()
         homeDir = expanduser('~')
         currentDir = dirname(activeView.file_name()) if activeView else homeDir
         self.panel = self.window.show_input_panel(
-            "Open file:",
+            "Open path:",
             join(currentDir, ''),
             self.on_done,
             self.on_change,
             None
         )
     def on_change(self, text):
+        self.panel.settings().set('tab_completion', False)
         if text.endswith('\t'):
             self.panel.run_command('complete_path', dict(text=text.strip()))
-
     def on_done(self, text):
         if isdir(text):
             window = self.window
