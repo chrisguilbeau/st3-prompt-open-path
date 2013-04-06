dbff574100b5e15684e885faa1efb3b60f45e623
Chris Guilbeau <chris.guilbeau@gmail.com>
2013-04-06 00:09:48 -0500

    incremental commit... - now has tab completion

--- st3-prompt-open-path.py
+++ st3-prompt-open-path.py
@@ -1,12 +1,38 @@
 # python imports
-from os.path import expanduser
+from os import listdir
 from os.path import dirname
+from os.path import expanduser
 from os.path import isdir
+from os.path import join
+from os.path import split
+from os.path import commonprefix
+from os import sep
+import rlcompleter
 
 # sublime imports
 from sublime_plugin import WindowCommand
+from sublime_plugin import TextCommand
 from sublime import error_message
 from sublime import message_dialog
+from sublime import status_message
+from sublime import Region
+
+class CompletePath(TextCommand):
+    def run(self, edit, text):
+        baseDir, partialPath = split(text)
+        panel = self.view
+        newText = join(baseDir, partialPath)
+        region = Region(0, panel.size())
+        options = [name for name in listdir(baseDir) if name.startswith(partialPath)]        
+        status_message(' ,'.join(options))
+        if not options:
+            newText = text
+        else:
+            newText = join(baseDir, commonprefix(options))
+        # Add a trailing slash if you are tabbing into a directory
+        if len(options) == 1 and isdir(newText):
+            newText = join(newText, '')
+        panel.replace(edit, region, newText)
 
 class St3PromptOpenPath(WindowCommand):
     '''Prompts for path of file or directory to open'''
@@ -17,18 +43,15 @@ class St3PromptOpenPath(WindowCommand):
         currentDir = dirname(activeView.file_name()) if activeView else homeDir
         self.panel = self.window.show_input_panel(
             "Open file:",
-            currentDir,
+            join(currentDir, ''),
             self.on_done,
             self.on_change,
             None
         )
     def on_change(self, text):
-        panel = self.panel
-        edit = panel.begin_edit()
-        allTextRegion = panel.full_line(0)
-        newPath = 'Chris is cool'
-        panel.replace(edit, allTextRegion, newPath)
-        panel.end_edit(edit)
+        if text.endswith('\t'):
+            self.panel.run_command('complete_path', dict(text=text.strip()))
+
     def on_done(self, text):
         if isdir(text):
             message_dialog('Can not open directories')
@@ -37,32 +60,3 @@ class St3PromptOpenPath(WindowCommand):
             self.window.open_file(text)
         except:
             error_message('Unable to open "%s"' % text)
-
-# class SetMabPathCommand(sublime_plugin.TextCommand):
-#     _input = None
-#     settings = sublime.load_settings('Mab.sublime-settings')
-
-#     def run(self, edit, key='', val='', save=True):
-#         if not self._input:
-#             path = self.settings.get('mab_path')
-#             self._input = sublime.active_window().show_input_panel('input',
-#                 path, self.on_done, self.on_change, self.on_close)
-#             self.on_change(path)
-
-#     def on_done(self, text):
-#         if os.path.exists(text):
-#             self.settings.set('mab_path', text)
-#             sublime.save_settings('Mab.sublime-settings')
-#         else:
-#             sublime.error_message('Path %s does not exists' % text)
-
-#     def on_close(self):
-#         self._input = None
-
-#     def on_change(self, text):
-#         if self._input:
-#             if os.path.exists(text):
-#                 scope = 'constant'
-#             else:
-#                 scope= 'constant.character.escape'
-#             self._input.add_regions('regs', [sublime.Region(0, self._input.size())], scope)
